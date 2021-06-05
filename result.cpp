426f700c0a91768d91a93acc9886182fc4417782
raghuaj <83672150+raghuaj@users.noreply.github.com>
2021-06-04 23:33:34 -0700

    Create result.cpp

---
+++ result.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	int marks;
+	cout <<"enter your marks "<<endl;
+	cin>>marks;
+	if(marks>=40)
+	{
+		cout <<"pass";
+	}
+	else{
+		cout<<"fail";
+	}
+	return 0;
+}
